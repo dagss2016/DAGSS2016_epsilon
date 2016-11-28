bb65d9746d0dc09ee0e433135816604f02701b98
robvalca <rvcameselle@esei.uvigo.es>
2016-11-28 19:46:16 +0100

    Trabajo en controlador y DAO de citas, y en vistas del medico.

---
+++ src/main/java/es/uvigo/esei/dagss/controladores/cita/CitaControlador.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uvigo.esei.dagss.controladores.cita;
+
+import es.uvigo.esei.dagss.controladores.medico.MedicoControlador;
+import es.uvigo.esei.dagss.dominio.daos.CitaDAO;
+import es.uvigo.esei.dagss.dominio.entidades.Cita;
+import es.uvigo.esei.dagss.dominio.entidades.Paciente;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+
+/**
+ *
+ * @author rbr
+ */
+@Named(value = "citaControlador")
+@SessionScoped
+public class CitaControlador implements Serializable {
+ 
+    List<Cita> citas;
+    Cita citaActual;
+    
+    private Integer id;
+    private String fecha;
+
+    @Inject
+    private MedicoControlador medicoControlador;
+  
+    @EJB
+    private CitaDAO citaDAO;
+
+    /**
+     * Creates a new instance of CitaControlador
+     */
+    public CitaControlador() {
+    }
+    
+    @PostConstruct
+    public void inicializar() {
+        this.citas = citaDAO.buscarTodosByMedico(medicoControlador.getMedicoActual().getId());
+    }
+    
+    public Cita getCitaActual() {
+        return citaActual;
+    }
+
+    public void setCitaActual(Cita citaActual) {
+        this.citaActual = citaActual;
+    }
+
+    private Cita recuperarDatosCita() {
+        Cita cita = null;
+        if (id != null) {
+            cita = citaDAO.buscarPorId(id);
+        }
+        /*if ((cita == null) && (numeroColegiado != null)) {
+            cita = citaDAO.buscarPorNumeroColegiado(numeroColegiado);
+        }*/
+        if ((cita == null) && (fecha != null)) {
+            cita = citaDAO.buscarPorFecha(fecha);
+        }
+        
+        return cita;
+    }    
+
+    public List<Cita> getCitas() {
+        return citas;
+    }
+
+    public void setCitas(List<Cita> citas) {
+        this.citas = citas;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
--- src/main/java/es/uvigo/esei/dagss/dominio/daos/CitaDAO.java
+++ src/main/java/es/uvigo/esei/dagss/dominio/daos/CitaDAO.java
@@ -5,13 +5,46 @@
 package es.uvigo.esei.dagss.dominio.daos;
 
 import es.uvigo.esei.dagss.dominio.entidades.Cita;
+//import es.uvigo.esei.dagss.dominio.entidades.Medico;
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 
 @Stateless
 @LocalBean
 public class CitaDAO  extends GenericoDAO<Cita>{    
+
+    public Cita buscarPorId(Integer id) {
+        TypedQuery<Cita> q = em.createQuery("SELECT m FROM Cita AS m "
+                                            + "  WHERE m.id = :id ", Cita.class);
+        q.setParameter("id", id);
+
+        return filtrarResultadoUnico(q);
+    }
+
+    public Cita buscarPorFecha(String fecha) {
+        TypedQuery<Cita> q = em.createQuery("SELECT m FROM Cita AS m "
+                                            + "  WHERE m.fecha = :fecha ", Cita.class);
+        q.setParameter("fecha", fecha);
+        
+        return filtrarResultadoUnico(q);
+    }
+
+    public List<Cita> buscarPorPaciente(Long paciente_Id) {
+        TypedQuery<Cita> q = em.createQuery("SELECT m FROM Cita AS m "
+                + "  WHERE m.paciente.id = :paciente_Id", Cita.class);
+        q.setParameter("paciente_Id",paciente_Id);        
+        return q.getResultList();
+    }
+    
+    public List<Cita> buscarTodosByMedico(Long medico_Id) {
+        TypedQuery<Cita> q = em.createQuery("SELECT m FROM Cita AS m "
+                + "  WHERE m.medico.id = :medico_Id", Cita.class);
+        q.setParameter("medico_Id",medico_Id);        
+        return q.getResultList();    
+    }
 
     // Completar aqui
 }
