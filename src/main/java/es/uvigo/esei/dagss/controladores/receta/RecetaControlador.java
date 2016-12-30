eee09f2c3272186285bc52932478f19fb4558a47
robvalca <rvcameselle@esei.uvigo.es>
2016-12-30 18:48:20 +0100

    - Creado controladorReceta y añadida opcion de buscar recetas por prescripcion (sólo lógica, no vistas).

---
+++ src/main/java/es/uvigo/esei/dagss/controladores/receta/RecetaControlador.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uvigo.esei.dagss.controladores.receta;
+
+import es.uvigo.esei.dagss.controladores.prescripcion.PrescripcionControlador;
+import es.uvigo.esei.dagss.dominio.daos.RecetaDAO;
+import es.uvigo.esei.dagss.dominio.entidades.Receta;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Inject;
+
+
+/**
+ *
+ * @author rbr
+ */
+@Named(value = "recetaControlador")
+@SessionScoped
+public class RecetaControlador implements Serializable {
+    
+    List<Receta> recetas;
+    Receta recetaActual;
+    
+    @Inject
+    private PrescripcionControlador prescripcionControlador;
+  
+    @EJB
+    private RecetaDAO recetaDAO;
+    
+    /**
+     * Creates a new instance of RecetaControlador
+     */
+    public RecetaControlador() {
+    }
+    
+    @PostConstruct
+    public void inicializar() {  
+        
+        this.recetas = recetaDAO.buscarTodos();
+    }
+    
+    public void recetasPrescripcion() {
+        
+        this.recetas = recetaDAO.buscarPorPrescripcion(prescripcionControlador.getPrescripcionActual().getId());
+    }
+    
+    public List<Receta> getRecetas() {
+        return recetas;
+    }
+
+    public void setRecetas(List<Receta> recetas) {
+        this.recetas = recetas;
+    }
+
+    public Receta getRecetaActual() {
+        return recetaActual;
+    }
+
+    public void setRecetaActual(Receta recetaActual) {
+        this.recetaActual = recetaActual;
+    }
+    
+    public void doEliminar() {
+        recetaDAO.eliminar(recetaActual);
+        //recetas = recetaDAO.buscarPorPrescripcion(prescripcionControlador.getPrescripcionActual().getId());
+        recetas = recetaDAO.buscarTodos();
+    }
+    
+    public void doNuevo() {
+        recetaActual = new Receta(); // Receta vacía
+        //recetaActual.setMedico(medicoControlador.getMedicoActual());
+        //recetaActual.setPaciente(citaControlador.getCitaActual().getPaciente());
+        /*medicoActual.setFechaAlta(Calendar.getInstance().getTime());
+        medicoActual.setUltimoAcceso(medicoActual.getFechaAlta());*/
+    }
+    
+    public void doEditar(Receta receta) {
+        recetaActual = receta;   // Otra alternativa: volver a refrescarlos desde el DAO
+    }
+    
+    public void doGuardarNuevo() {  
+        recetaActual = recetaDAO.crear(recetaActual);
+        // Actualiza lista 
+        //recetas = recetaDAO.buscarPorPrescripcion(prescripcionControlador.getPrescripcionActual().getId());
+        recetas = recetaDAO.buscarTodos();
+    }
+    
+    public void doGuardarEditado() {
+        //Comprobaciones
+        recetaActual = recetaDAO.actualizar(recetaActual);
+        //recetas = recetaDAO.buscarPorPrescripcion(prescripcionControlador.getPrescripcionActual().getId());
+        recetas = recetaDAO.buscarTodos();
+    }
+    
+    public String doVolver() {
+        return "../index?faces-redirect=true";
+    }
+}
--- src/main/java/es/uvigo/esei/dagss/dominio/daos/RecetaDAO.java
+++ src/main/java/es/uvigo/esei/dagss/dominio/daos/RecetaDAO.java
@@ -5,12 +5,21 @@
 package es.uvigo.esei.dagss.dominio.daos;
 
 import es.uvigo.esei.dagss.dominio.entidades.Receta;
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 @Stateless
 @LocalBean
 public class RecetaDAO extends GenericoDAO<Receta>{
+
+    public List<Receta> buscarPorPrescripcion(Long id) {
+         TypedQuery<Receta> q = em.createQuery("SELECT r FROM Receta AS r "
+                + "  WHERE r.prescripcion.id = :prescripcion_Id", Receta.class);
+        q.setParameter("prescripcion_Id",id);        
+        return q.getResultList();
+    }
  
     // Completar aqui
 }
